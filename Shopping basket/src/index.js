553ed5d4c8155823abccdd64e1cb8ddaa139bfb3
Ibrahim Ali <112340231+IbrahimXYZ@users.noreply.github.com>
2024-09-26 09:03:05 -0700

    Update index.js

--- Shopping basket/src/index.js
+++ Shopping basket/src/index.js
@@ -4,19 +4,22 @@ const OfferMain = require("./offers/OfferMain")
 const BasketPricer = require("./pricer/basketPricer")
 const {BuyXGetYFree, PercentageDiscount } = require("./offers/offers")
 
-
+// intitalizing and adding items to catalogue
 const catalogue = new Catalogue();
 catalogue.addItem("eggs", 1.50)
 catalogue.addItem("milk" , 1.20)
 
+//intializing and adding items to basket
 const basket = new Basket();
 basket.addItem("eggs",3)
 basket.addItem("milk",1)
 
+//applying offers to which items
 const offerMain = new OfferMain();
 offerMain.addOffer(new BuyXGetYFree("eggs",2,1));
 offerMain.addOffer(new PercentageDiscount("milk",10))
 
+//running basketpricer to calculate different costs
 const basketPricer = new BasketPricer(basket,catalogue,offerMain)
 
 const pricing = basketPricer.calculateTotal();
